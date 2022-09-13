a403d4feb0a1bb22e57a662901f36c298af6cb2f
mshruler <80628428+mshruler@users.noreply.github.com>
2022-09-13 11:27:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,17 +31,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "",
+      name: "佳佳",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "owYNU6PGmE0TQU7pQtM2WJLmOMUE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "Ey6qVCMZYHLdU-zRLJPgcAcVlU093-vTl_BCSYpIOXw",
       // 所在省份
-      province: "",
+      province: "河南",
       // 所在城市
-      city: "",
+      city: "项城",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-22',
+      horoscopeDate: '10-03',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -49,15 +49,15 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "亲爱的老婆", "year": "2006", "date": "12-22"},
-        {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "09-07"},
+        {"type": "*生日", "name": "亲爱的老婆", "year": "2004", "date": "10-03"},
+        {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "09-11"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "你老公", "year": "2001", "date": "08-01"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-07"},
+        {"keyword": "love_day", date: "2022-09-11"},
       ]
     },
     {
